450c599eea4830de1b9281daaf38272d631d3335
willbillionquant <willbillion2017@gmail.com>
2022-12-02 20:14:25 +0800

    Create nycalendar.py

---
+++ nycalendar.py
@@ -0,0 +1,10 @@
+import os
+codepath_td = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append('..')
+
+from datetime import datetime
+from itertools import product
+
+from tdcalendar import *
+
