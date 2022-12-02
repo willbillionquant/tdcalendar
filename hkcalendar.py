fabee318ec8fbae5532bfcbc5b8e6df9854e22c9
willbillionquant <willbillion2017@gmail.com>
2022-12-02 20:14:22 +0800

    Create hkcalendar.py

---
+++ hkcalendar.py
@@ -0,0 +1,12 @@
+import os
+codepath_td = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append('..')
+
+from datetime import datetime
+from itertools import product
+
+from tdcalendar import *
+
+
+
