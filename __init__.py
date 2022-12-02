22f89052c2e1b660d5721b9409aa372fc84e748f
willbillionquant <willbillion2017@gmail.com>
2022-12-02 19:53:05 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,16 +4,73 @@ import sys
 sys.path.append('..')
 
 from datetime import datetime, timedelta
+import pandas as pd
+
+def getnyholidays(startyear=1990, endyear=2046):
+    """Obtain US stock market full-day holidays."""
+    dfhday_ny = pd.DataFrame()
+
+    for year in range(startyear, endyear + 1):
+        try:
+            tablelist = pd.read_html(f'http://www.market-holidays.com/{year}')
+            dfraw = tablelist[0].copy()
+            dfhday = pd.DataFrame(columns=['date', 'wday', 'name'])
+            dfhday['date'] = dfraw[1].apply(lambda x: datetime.strptime(x, '%B %d, %Y'))
+            dfhday['wday'] = dfhday['date'].apply(lambda x: x.weekday() + 1)
+            dfhday['name'] = dfraw[0]
+            dfhday_ny = pd.concat([dfhday_ny, dfhday], axis=0)
+            print(f'{year} US holiday done.')
+        except:
+            print(f'{year} US holiday not available.')
+            break
+
+    dfhday_ny.set_index('date', inplace=True)
+    dfhday_ny['name'] = dfhday_ny['name'].replace('Martin Luther King, Jr. Day', 'MLK Day')\
+                                         .replace('Juneteenth National Independence Day', '619 Day')
+    dfhday_ny = dfhday_ny[dfhday_ny['name'] != 'Martin Luther King, Jr. Day(1-minute pause at noon)']
+    dfhday_ny.to_csv(os.path.join(codepath_td, 'holiday_ny.csv'))
+
+
+def gethkholidays(startyear=2007, endyear=2046):
+    """Obtain US stock market full-day holidays."""
+    dfhday_hk = pd.DataFrame()
+
+    for year in range(startyear, endyear + 1):
+        try:
+            tablelist = pd.read_html(f'https://www.gov.hk/en/about/abouthk/holiday/{year}.htm')
+            dfraw = tablelist[0].loc[1:]
+            dfhday = pd.DataFrame(columns=['date', 'wday', 'name'])
+            dfhday['date'] = dfraw[1].apply(lambda x: datetime.strptime(x + f',{year}', '%d %B,%Y'))
+            dfhday['wday'] = dfhday['date'].apply(lambda x: x.weekday() + 1)
+            dfhday['name'] = dfraw[0]
+            dfhday_hk = pd.concat([dfhday_hk, dfhday], axis=0)
+            print(f'{year} HK holiday done.')
+        except:
+            print(f'{year} HK holiday not available.')
+            break
+
+    dfhday_hk.set_index('date', inplace=True)
+    dfhday_hk.to_csv('holiday_hk.csv')
 
 # US stock market holidays
-holidayfile_ny = open(os.path.join(codepath_td, 'holiday_ny.csv'), 'r')
-holidaylines_ny = holidayfile_ny.readlines()
-holidaylist_ny = [datetime.strptime(row.split(',')[0], '%Y-%m-%d') for row in holidaylines_ny[1:]]
+holidayfile_ny = os.path.join(codepath_td, 'holiday_ny.csv')
+
+if not os.path.exists(holidayfile_ny):
+    getnyholidays()
+
+with open(holidayfile_ny, 'r') as f:
+    holidaylines_ny = f.readlines()
+    holidaylist_ny = [datetime.strptime(row.split(',')[0], '%Y-%m-%d') for row in holidaylines_ny[1:]]
 
 # HK stock market holidays
-holidayfile_hk = open(os.path.join(codepath_td, 'holiday_hk.csv'), 'r')
-holidaylines_hk = holidayfile_hk.readlines()
-holidaylist_hk = [datetime.strptime(row.split(',')[0], '%Y-%m-%d') for row in holidaylines_hk[1:]]
+holidayfile_hk = os.path.join(codepath_td, 'holiday_hk.csv')
+
+if not os.path.exists(holidayfile_hk):
+    gethkholidays()
+
+with open(holidayfile_hk, 'r') as f:
+    holidaylines_hk = f.readlines()
+    holidaylist_hk = [datetime.strptime(row.split(',')[0], '%Y-%m-%d') for row in holidaylines_hk[1:]]
 
 # List of all weekdays
 def getwkdays(startyr=1997, endyr=2046, form='%Y-%m-%d'):
