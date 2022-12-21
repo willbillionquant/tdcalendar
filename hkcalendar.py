8d39a87f8802b6c780f0a127dc09c3b3ff24915e
willbillionquant <willbillion2017@gmail.com>
2022-12-21 14:51:54 +0800

    Update hkcalendar.py

--- hkcalendar.py
+++ hkcalendar.py
@@ -8,15 +8,24 @@ from itertools import product
 
 from tdcalendar import *
 
-def gethkmonthexpiry(monthstr='JAN-21'):
+def gethkmonthexpiry(month='JAN-21'):
     """Obtain HK monthly FOPs expiry, defined as 2nd last HK trading day in a calendar month."""
-    monthstart = datetime.strptime(monthstr, '%b-%y')
-    monthstr = monthstart.strftime('%Y-%m')
+    if type(month) == tuple:  # e.g. (2021, 1)
+        monthstart = datetime(month[0], month[1], 1)
+        month = monthstart.strftime('%Y-%m')
+    elif type(month) == str:
+        if len(month) == 6: # e.g. JAN-21
+            monthstart = datetime.strptime(month, '%b-%y')
+            month = monthstart.strftime('%Y-%m')
+        else:  # e.g. 2021-01
+            monthstart = datetime.strptime(f'{month}-01', '%Y-%m-%d')
+    else:
+        raise AttributeError(f'Input for month must be %b-%y, %Y-%m or (%Y, %m)')
     year = monthstart.year
     holidaylistyr = [date for date in holidaylist_hk if date.year == year]
     if holidaylistyr != []:
         tdaylist = gettradedays(holidaylist_hk, datetime(year, 1, 1), datetime(year, 12, 31))
-        tdaylist = [dtstr for dtstr in tdaylist if dtstr[:7] == monthstr]
+        tdaylist = [dtstr for dtstr in tdaylist if dtstr[:7] == month]
         return tdaylist[-2]
     else:
         raise AttributeError(f'HK Holiday for year {year} NOT available!')
